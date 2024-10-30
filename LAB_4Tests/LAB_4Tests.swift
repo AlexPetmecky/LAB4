0c75fc0bb12bc00e3b7ced37bfe4121f51f9fe47
alexpetmecky <petmeckyalex@gmail.com>
2024-10-30 12:33:20 -0500

    Initial Commit

---
+++ LAB_4Tests/LAB_4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LAB_4Tests.swift
+//  LAB_4Tests
+//
+//  Created by Alex Petmecky on 10/30/24.
+//
+
+import Testing
+@testable import LAB_4
+
+struct LAB_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
